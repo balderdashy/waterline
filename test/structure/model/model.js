8cff0095c8829fc3a1820f04b31789c970d83eb3
Cody Stoltman <particlebanana@gmail.com>
2013-05-11 02:10:00 -0500

    placeholder for instance of a model

---
+++ test/structure/model/model.js
@@ -0,0 +1,21 @@
+var Model = require('../../../lib/model'),
+    assert = require('assert');
+
+describe('Model', function() {
+
+  it('should allow the prototype to be extended', function() {
+    var Person = Model.extend({ foo: 'bar' });
+    var person = new Person();
+
+    assert(person.persisted === false);
+    assert(person.foo === 'bar');
+  });
+
+  it('should add properties to new instance of model', function() {
+    var Person = Model.extend({});
+    var person = new Person({ name: 'Foo Bar' });
+
+    assert(person.name === 'Foo Bar');
+  });
+
+});
