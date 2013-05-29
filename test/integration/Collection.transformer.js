26fb2742586afa859a13f368130467fcd5c4b38e
Cody Stoltman <particlebanana@gmail.com>
2013-05-28 22:41:52 -0500

    add Transformer to Core

---
+++ test/integration/Collection.transformer.js
@@ -0,0 +1,31 @@
+var Waterline = require('../../lib/waterline'),
+    assert = require('assert');
+
+describe('Waterline Collection', function() {
+
+  describe('with custom column name', function() {
+    var User;
+
+    before(function(done) {
+      var Model = Waterline.Model.extend({
+        attributes: {
+          name: {
+            type: 'string',
+            columnName: 'full_name'
+          }
+        }
+      });
+
+      new Model({ tableName: 'foo' }, function(err, collection) {
+        if(err) return done(err);
+        User = collection;
+        done();
+      });
+    });
+
+    it('should build a transformer object', function() {
+      assert(User._transformer._transformations.name === 'full_name');
+    });
+  });
+
+});
