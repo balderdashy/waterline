6856cf469227f5d3973ea0ff0896cd15736d9c05
Cody Stoltman <particlebanana@gmail.com>
2013-06-07 12:21:28 -0500

    add validation tests for length

---
+++ test/unit/validations/validations.length.js
@@ -0,0 +1,61 @@
+var Validator = require('../../../lib/waterline/core/validations'),
+    assert = require('assert');
+
+describe('validations', function() {
+
+  describe('lengths', function() {
+    var validator;
+
+    before(function() {
+
+      var validations = {
+        firstName: {
+          type: 'string',
+          minLength: 2
+        },
+        lastName: {
+          type: 'string',
+          maxLength: 5
+        }
+      };
+
+      validator = new Validator();
+      validator.build(validations);
+    });
+
+    describe('minLength', function() {
+
+      it('should validate minLength', function(done) {
+        validator.validate({ firstName: 'foo' }, function(errors) {
+          assert(!errors);
+          done();
+        });
+      });
+
+      it('should error if length is shorter', function(done) {
+        validator.validate({ firstName: 'f' }, function(errors) {
+          assert(errors.firstName);
+          done();
+        });
+      });
+    });
+
+    describe('maxLength', function() {
+
+      it('should validate maxLength', function(done) {
+        validator.validate({ lastName: 'foo' }, function(errors) {
+          assert(!errors);
+          done();
+        });
+      });
+
+      it('should error if length is longer', function(done) {
+        validator.validate({ lastName: 'foobar' }, function(errors) {
+          assert(errors.lastName);
+          done();
+        });
+      });
+    });
+
+  });
+});
