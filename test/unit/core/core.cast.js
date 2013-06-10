6edc447dff2131e1e84b746f45ee15ba5d8ca988
Cody Stoltman <particlebanana@gmail.com>
2013-06-10 15:12:51 -0500

    add number type casting
    - needed for mongo really so numbers get stored correctly when using the
    blueprints in Sails

---
+++ test/unit/core/core.cast.js
@@ -0,0 +1,35 @@
+var Core = require('../../../lib/waterline/core'),
+    assert = require('assert');
+
+describe('Core Type Casting', function() {
+
+  describe('.cast() with model attributes', function() {
+    var person;
+
+    before(function() {
+      var Person = Core.extend({
+        attributes: {
+          name: {
+            type: 'string'
+          },
+          age: {
+            type: 'integer'
+          }
+        }
+      });
+
+      person = new Person();
+    });
+
+    it('should cast values to proper types', function() {
+      var values = person._cast.run({ name: '27', age: '27' });
+
+      assert(typeof values.name === 'string');
+      assert(values.name === '27');
+
+      assert(typeof values.age === 'number');
+      assert(values.age === 27);
+    });
+
+  });
+});
