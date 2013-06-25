c68b7f0a39fcfe9e2dd6407ca1b8bee15a9ef9f1
Cody Stoltman <particlebanana@gmail.com>
2013-06-25 18:53:03 -0500

    fix: error with uppercase types. closes #35

--- test/integration/Collection.attributes.js
+++ test/integration/Collection.attributes.js
@@ -1,45 +1,108 @@
-var Model = require('./fixtures/model.fixture'),
+var Waterline = require('../../lib/waterline'),
     assert = require('assert');
 
 describe('Waterline Collection', function() {
-  var User;
 
-  before(function(done) {
-    new Model(function(err, collection) {
-      if(err) return done(err);
-      User = collection;
-      done();
-    });
-  });
+  describe('basic fixture', function() {
+    var Model = require('./fixtures/model.fixture'),
+        User;
 
-  describe('schema', function() {
-
-    it('should create an internal schema from the attributes', function() {
-      assert(typeof User._schema.schema === 'object');
-      assert(Object.keys(User._schema.schema).length === 8); // account for auto created keys (pk, timestamps)
+    before(function(done) {
+      new Model(function(err, collection) {
+        if(err) return done(err);
+        User = collection;
+        done();
+      });
     });
 
-    // TO-DO
-    // Check all schema properties from Sails work
+    describe('schema', function() {
 
-  });
+      it('should create an internal schema from the attributes', function() {
+        assert(typeof User._schema.schema === 'object');
+        assert(Object.keys(User._schema.schema).length === 8); // account for auto created keys (pk, timestamps)
+      });
 
-  describe('validations', function() {
+      // TO-DO
+      // Check all schema properties from Sails work
 
-    it('should create an internal validation object from the attributes', function() {
-      assert(typeof User._validator.validations === 'object');
-      assert(Object.keys(User._validator.validations).length === 5);
     });
 
-    // TO-DO
-    // Validate properties using Anchor with the Validator in waterline
+    describe('validations', function() {
+
+      it('should create an internal validation object from the attributes', function() {
+        assert(typeof User._validator.validations === 'object');
+        assert(Object.keys(User._validator.validations).length === 5);
+      });
+
+      // TO-DO
+      // Validate properties using Anchor with the Validator in waterline
+
+    });
+
+    describe('instance methods', function() {
+
+      it('should create an internal instanceMethods object from the attributes', function() {
+        assert(typeof User._instanceMethods === 'object');
+      });
+
+    });
 
   });
 
-  describe('instance methods', function() {
+  describe('custom fixtures', function() {
 
-    it('should create an internal instanceMethods object from the attributes', function() {
-      assert(typeof User._instanceMethods === 'object');
+    describe('lowercase type', function() {
+      var User;
+
+      before(function(done) {
+        var Model = Waterline.Collection.extend({
+          tableName: 'lowercaseType',
+          attributes: {
+            name: 'string'
+          }
+        });
+
+        new Model(function(err, collection) {
+          if(err) return done(err);
+          User = collection;
+          done();
+        });
+      });
+
+      it('should set the proper schema type', function() {
+        assert(User._schema.schema.name.type === 'string');
+      });
+
+      it('should set the proper validation type', function() {
+        assert(User._validator.validations.name.type === 'string');
+      });
+    });
+
+    describe('uppercase type', function() {
+      var User;
+
+      before(function(done) {
+        var Model = Waterline.Collection.extend({
+          tableName: 'uppercaseType',
+          attributes: {
+            name: 'STRING'
+          }
+        });
+
+        new Model(function(err, collection) {
+          if(err) return done(err);
+          User = collection;
+          done();
+        });
+      });
+
+      it('should set the proper schema', function() {
+        assert(User._schema.schema.name.type === 'string');
+      });
+
+      it('should set the proper validation type', function() {
+        assert(User._validator.validations.name.type === 'string');
+      });
     });
 
   });
