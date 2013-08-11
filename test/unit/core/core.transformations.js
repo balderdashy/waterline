67c3ee53a5335d1107f4b0e534d0de4ef5dc6d24
Cody Stoltman <particlebanana@gmail.com>
2013-08-11 16:22:33 -0500

    transform data with namespaced keys from a join query

--- test/unit/core/core.transformations.js
+++ test/unit/core/core.transformations.js
@@ -108,7 +108,6 @@ describe('Core Transformations', function() {
   });
 
   describe('unserialize', function() {
-    var transformer;
 
     before(function() {
       var attributes = {
@@ -121,15 +120,50 @@ describe('Core Transformations', function() {
         }
       };
 
-      transformer = new Transformer(attributes);
+        transformer = new Transformer(attributes);
+      });
+
+      it('should change login key to username', function() {
+        var values = transformer.unserialize({ login: 'foo' });
+        assert(values.username);
+        assert(values.username === 'foo');
+      });
     });
 
-    it('should change login key to username', function() {
-      var values = transformer.unserialize({ login: 'foo' });
-      assert(values.username);
-      assert(values.username === 'foo');
+    describe('with associations', function() {
+      var transformer;
+
+      before(function() {
+        var attributes = {
+          name: 'string',
+          car: {
+            model: 'Car'
+          }
+        };
+
+        transformer = new Transformer(attributes);
+      });
+
+      it('should change car_id key to car', function() {
+        var values = transformer.unserialize({ car_id: 1 });
+        assert(values.car);
+        assert(!values.car_id);
+        assert(values.car === 1);
+      });
+
+      it('should group results prefixed with special `__` character sequence', function() {
+        var values = transformer.unserialize({
+          name: 'foo',
+          car__id: 1,
+          car__make: 'porsche',
+          car__model: 'cayman'
+        });
+
+        assert(values.car);
+        assert(Object.keys(values.car).length === 3);
+      });
     });
-    
+
     it('shouldnt delete password key when it matches the column name', function() {
       var values = transformer.unserialize({ password: 'bar' });
       assert(values.password);
