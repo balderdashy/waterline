4cb30edfb4eb6e43e91eada882754d729968668c
Cody Stoltman <particlebanana@gmail.com>
2013-08-25 18:35:10 -0500

    map out joins when populate is used

---
+++ test/unit/query/associations/belongsTo.js
@@ -0,0 +1,64 @@
+var Waterline = require('../../../../lib/waterline'),
+    assert = require('assert');
+
+describe('Collection Query', function() {
+  describe('belongs to association', function() {
+    var Car;
+
+    before(function(done) {
+
+      var waterline = new Waterline();
+      var collections = {};
+
+      collections.user = Waterline.Collection.extend({
+        identity: 'user',
+        adapter: 'foo',
+        attributes: {
+          uuid: {
+            type: 'string',
+            primaryKey: true
+          },
+          name: {
+            type: 'string',
+            defaultsTo: 'Foo Bar'
+          }
+        }
+      });
+
+      collections.car = Waterline.Collection.extend({
+        identity: 'car',
+        adapter: 'foo',
+        attributes: {
+          driver: {
+            model: 'user'
+          }
+        }
+      });
+
+      waterline.loadCollection(collections.user);
+      waterline.loadCollection(collections.car);
+
+      // Fixture Adapter Def
+      var adapterDef = { find: function(col, criteria, cb) { return cb(null, [criteria]); }};
+
+      waterline.initialize({ adapters: { foo: adapterDef }}, function(err, colls) {
+        if(err) done(err);
+        Car = colls.car;
+        done();
+      });
+    });
+
+
+    it('should build a join query', function(done) {
+      Car.findOne({ driver: 1 })
+      .populate('driver')
+      .exec(function(err, values) {
+        assert(values.joins[0].collection === 'user');
+        assert(values.joins[0].pk === 'id');
+        assert(values.joins[0].fk === 'uuid');
+        done();
+      });
+    });
+
+  });
+});
