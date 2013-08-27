d198e144fd03ffc1a9ff30e17da77014ba18c0e5
Cody Stoltman <particlebanana@gmail.com>
2013-08-26 19:25:59 -0500

    add getters and setters for the has many api

--- test/structure/model/model.js
+++ test/structure/model/model.js
@@ -4,14 +4,14 @@ var Model = require('../../../lib/waterline/model'),
 describe('Model', function() {
 
   it('should allow the prototype to be extended', function() {
-    var Person = Model.extend({ foo: 'bar' });
-    var person = new Person();
+    var Person = Model.inject({}, { foo: 'bar' });
+    var person = new Person({});
 
     assert(person.foo === 'bar');
   });
 
   it('should add properties to new instance of model', function() {
-    var Person = Model.extend({});
+    var Person = Model.inject({},{});
     var person = new Person({ name: 'Foo Bar' });
 
     assert(person.name === 'Foo Bar');
