a8e4a08b1c05b4ede9144c8dca36bb213da26bb5
Cody Stoltman <particlebanana@gmail.com>
2013-08-28 16:44:15 -0500

    add an optional param for showing join keys to a model instance

--- test/unit/model/model.toObject.js
+++ test/unit/model/model.toObject.js
@@ -8,45 +8,111 @@ var Waterline = require('../../../lib/waterline'),
 describe('Model', function() {
 
   describe('.toObject()', function() {
-    var collection;
 
-    /**
-     * Build a test model
-     *
-     * Uses an empty adapter definition
-     */
+    describe('with instance method', function() {
+      var collection;
 
-    before(function(done) {
-      var waterline = new Waterline();
-      var Model = Waterline.Collection.extend({
-        adapter: 'foo',
-        tableName: 'person',
-        attributes: {
-          first_name: 'string',
-          last_name: 'string',
-          full_name: function() {
-            return this.first_name + ' ' + this.last_name;
+      /**
+       * Build a test model
+       *
+       * Uses an empty adapter definition
+       */
+
+      before(function(done) {
+        var waterline = new Waterline();
+        var Model = Waterline.Collection.extend({
+          adapter: 'foo',
+          tableName: 'person',
+          attributes: {
+            first_name: 'string',
+            last_name: 'string',
+            full_name: function() {
+              return this.first_name + ' ' + this.last_name;
+            }
           }
-        }
+        });
+
+        waterline.loadCollection(Model);
+
+        waterline.initialize({ adapters: { foo: {} }}, function(err, colls) {
+          if(err) done(err);
+          collection = colls.person;
+          done();
+        });
       });
 
-      waterline.loadCollection(Model);
+      it('should strip out the instance methods', function() {
+        var person = new collection._model({ first_name: 'foo', last_name: 'bar' });
+        var obj = person.toObject();
 
-      waterline.initialize({ adapters: { foo: {} }}, function(err, colls) {
-        if(err) done(err);
-        collection = colls.person;
-        done();
+        assert(obj === Object(obj));
+        assert(obj.first_name === 'foo');
+        assert(obj.last_name === 'bar');
+        assert(!obj.full_name);
       });
     });
 
-    it('should strip out the instance methods', function() {
-      var person = new collection._model({ first_name: 'foo', last_name: 'bar' });
-      var obj = person.toObject();
 
-      assert(obj === Object(obj));
-      assert(obj.first_name === 'foo');
-      assert(obj.last_name === 'bar');
-      assert(!obj.full_name);
+    describe('with associations', function() {
+      var collection;
+
+      /**
+       * Build a test model
+       *
+       * Uses an empty adapter definition
+       */
+
+      before(function(done) {
+        var waterline = new Waterline();
+        var Foo = Waterline.Collection.extend({
+          adapter: 'foo',
+          tableName: 'foo',
+          attributes: {
+            name: 'string',
+            bars: {
+              collection: 'bar'
+            }
+          }
+        });
+
+        var Bar = Waterline.Collection.extend({
+          adapter: 'foo',
+          tableName: 'bar',
+          attributes: {
+            name: 'string',
+            foo: {
+              model: 'foo'
+            }
+          }
+        });
+
+        waterline.loadCollection(Foo);
+        waterline.loadCollection(Bar);
+
+        waterline.initialize({ adapters: { foo: {} }}, function(err, colls) {
+          if(err) done(err);
+          collection = colls.foo;
+          done();
+        });
+      });
+
+      it('should strip out the association key when no options are passed', function() {
+        var person = new collection._model({ name: 'foobar' });
+        var obj = person.toObject();
+
+        assert(obj === Object(obj));
+        assert(obj.name === 'foobar');
+        assert(!obj.bars);
+      });
+
+      it('should keep the association key when showJoins option is passed', function() {
+        var person = new collection._model({ name: 'foobar' }, { showJoins: true });
+        var obj = person.toObject();
+
+        assert(obj === Object(obj));
+        assert(obj.name === 'foobar');
+        assert(obj.bars);
+      });
     });
 
   });
