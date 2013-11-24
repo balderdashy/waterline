3e8bde22fd548f6a3014cf5961f006e82b49102d
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2013-11-24 15:31:21 +0100

    Add soft delete
    A beginning for soft deletes, fixes
    https://github.com/balderdashy/waterline/issues/134.

--- test/unit/model/model.destroy.js
+++ test/unit/model/model.destroy.js
@@ -45,5 +45,38 @@ describe('Model', function() {
       });
     });
 
+    it('should set deletedAt and not delete the record', function(done) {
+      var Model = Waterline.Collection.extend({
+        adapter: 'foo',
+        tableName: 'person',
+        softDelete: true,
+        attributes: {
+          first_name: 'string',
+          last_name: 'string',
+          full_name: function() {
+            return this.first_name + ' ' + this.last_name;
+          }
+        }
+      });
+
+      var adapterDef = {
+        destroy: function(col, options, cb) { return cb(null, true); },
+        update: function(col, options, values, cb) { return cb(null, true); }
+      };
+      new Model({ adapters: { foo: adapterDef }}, function(err, coll) {
+        if(err) done(err);
+        collection = coll;
+
+        var person = new collection._model({ id: 1, first_name: 'foo', last_name: 'bar' });
+
+        person.destroy(function(err, status) {
+          assert(!err);
+          assert(status === true);
+          done();
+        });
+      });
+
+    });
+
   });
 });
