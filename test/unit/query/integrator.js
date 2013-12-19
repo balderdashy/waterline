f0ba1525e728c6847c48ca250b5acd4e329cacdb
Mike McNeil <michael.r.mcneil@gmail.com>
2013-12-19 15:37:53 -0600

    More tests.

--- test/unit/query/integrator.js
+++ test/unit/query/integrator.js
@@ -1,52 +1,67 @@
 /**
  * Module dependencies
  */
-var tables = require('../../support/fixtures/integrator/tables');
+var fixtures = {
+	joins: require('../../support/fixtures/integrator/joins'),
+	cache: require('../../support/fixtures/integrator/cache')
+};
+var integrate = require('../../../lib/waterline/query/integrator');
+var assert = require('assert');
+var should = require('should');
+
+
+
+
 
 
 describe('integrator', function () {
 
-	var cache, joins;
+	describe('with no callback', function () {
 
-	// Stub the cache and some join instructions
-	before(function () {
-
-		cache = new SubqueryCache();
-		_.extend(cache, {
-			user: tables.user,
-			message: tables.message,
-			message_to_user: tables.message_to_user,
-			message_cc_user: tables.message_cc_user,
-			message_bcc_user: tables.message_bcc_user
+		it('should throw', function () {
+			assert.throws(function () {
+				integrate({}, []);
+			});
 		});
-
-		joins = [{
-			where: {/* ... */},
-			select: [ /* ... */ ],
-			limit: undefined,
-			skip: undefined,
-			joins: [/* ... */],
-			parent: 'user',				// left table name
-			parentKey: 'id',			// left table primary key
-			child: 'message_to_user',	// right table name
-			childKey: 'user_id'			// right table foreign key
-		},
-		{
-			parent: 'message_to_user',	// left table name
-			parentKey: 'message_id',	// left table foreign key
-			child: 'message',			// right table name
-			childKey: 'id'				// right table primary key
-		}];
 	});
 
 
 
-	it('should populate the aliases specified in `joins`');
+	describe('with otherwise-invalid input', function () {
 
-	describe('populated alias', function (){
-		
-		it('should contain an array of objects, where each one is exactly the same as its source object in the cache');
-		
+		it('should trigger cb(err)', function (done) {
+			assert.doesNotThrow(function () {
+				integrate('foo', 'bar', function (err, results) {
+					assert(err);
+					done();
+				});
+			});
+		});
+	});
+
+
+
+	describe('with valid input', function () {
+
+		describe(':: results',function () {
+
+			it('should be an array', function (done) {
+				assert.doesNotThrow(function () {
+					integrate(fixtures.cache, fixtures.joins, function (err, results) {
+						assert(!err);
+						results.should.be.Array;
+						done(err);
+					});
+				});
+			});
+
+			it('should have items which have the properties of the parent table');
+
+			describe(':: populated aliases', function () {
+				it('should exist for every alias specified in `joins` (i.e. every `populate()`)');
+				it('should contain an array of objects, where each one is exactly the same as its source object in the cache');
+			});
+		});
 	});
 
 });
