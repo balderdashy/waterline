d7f560c0e7ca0e5d977790aa82d8e6e39ab050e4
Mike McNeil <michael.r.mcneil@gmail.com>
2013-12-24 15:55:22 -0600

    Organized the fixture data.

---
+++ test/support/fixtures/integrator/joinResults.js
@@ -0,0 +1,60 @@
+/**
+ * Results of joins between our tables of test data.
+ * @type {Object}
+ */
+module.exports = {
+
+  // Left outer join:
+  message___message_to_user: [{
+    id: 10,
+    user_id: 2,
+    subject: 'msgA',
+    body: 'A test message.',
+    from: 1
+  }, {
+    id: 10,
+    user_id: 3,
+    subject: 'msgA',
+    body: 'A test message.',
+    from: 1
+  }, {
+    id: 20,
+    subject: 'msgB',
+    body: 'Another test message.',
+    from: 1
+  }, {
+    id: 30,
+    subject: 'msgC',
+    body: 'Aint sent this one yet.',
+    from: null
+  }],
+
+
+
+  // Two left outer joins:
+  message___message_to_user___user: [{
+    email: 'a@recipient.com',
+    id: 10,
+    user_id: 2,
+    subject: 'msgA',
+    body: 'A test message.',
+    from: 1
+  }, {
+    email: 'b@recipient.com',
+    id: 10,
+    user_id: 3,
+    subject: 'msgA',
+    body: 'A test message.',
+    from: 1
+  }, {
+    id: 20,
+    subject: 'msgB',
+    body: 'Another test message.',
+    from: 1
+  }, {
+    id: 30,
+    subject: 'msgC',
+    body: 'Aint sent this one yet.',
+    from: null
+  }]
+};
