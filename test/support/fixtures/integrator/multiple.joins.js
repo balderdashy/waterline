98fd1cb28c910f13c072570ebe803f37aefa47d7
Mike McNeil <michael.r.mcneil@gmail.com>
2013-12-24 01:24:00 -0600

    Adds multiple association/populate test fixture and improves comments in fixtures to more accurately reflect the meaning of join operation chains.

---
+++ test/support/fixtures/integrator/multiple.joins.js
@@ -0,0 +1,42 @@
+/**
+ * Joins
+ * 
+ * @type {Array}
+ */
+module.exports = [
+	{
+		alias: 'to',				// the `alias` -- e.g. name of association
+		parent: 'message',			// left table name
+		parentKey: 'id',			// left table key
+		child: 'message_to_user',	// right table name
+		childKey: 'message_id'		// right table key
+	},
+	{
+		alias: 'to',				// the `alias` -- e.g. name of association
+		parent: 'message_to_user',	// left table name
+		parentKey: 'user_id',		// left table key
+		child: 'user',				// right table name
+		childKey: 'id'				// right table key
+	},
+	{
+		alias: 'from',				// the `alias` -- e.g. name of association
+		parent: 'message',			// left table name
+		parentKey: 'from',			// left table key
+		child: 'user',				// right table name
+		childKey: 'id'				// right table key
+	},
+	{
+		alias: 'cc',				// the `alias` -- e.g. name of association
+		parent: 'message',			// left table name
+		parentKey: 'id',			// left table key
+		child: 'message_cc_user',	// right table name
+		childKey: 'message_id'		// right table key
+	},
+	{
+		alias: 'cc',				// the `alias` -- e.g. name of association
+		parent: 'message_cc_user',	// left table name
+		parentKey: 'user_id',		// left table key
+		child: 'user',				// right table name
+		childKey: 'id'				// right table key
+	},
+];
--- test/support/fixtures/integrator/n..m.joins.js
+++ test/support/fixtures/integrator/n..m.joins.js
@@ -7,15 +7,15 @@ module.exports = [
 	{
 		alias: 'to',				// the `alias` -- e.g. name of association
 		parent: 'message',			// left table name
-		parentKey: 'id',			// left table primary key
+		parentKey: 'id',			// left table key
 		child: 'message_to_user',	// right table name
-		childKey: 'message_id'		// right table foreign key
+		childKey: 'message_id'		// right table key
 	},
 	{
 		alias: 'to',				// the `alias` -- e.g. name of association
 		parent: 'message_to_user',	// left table name
-		parentKey: 'user_id',		// left table foreign key
+		parentKey: 'user_id',		// left table key
 		child: 'user',				// right table name
-		childKey: 'id'				// right table primary key
+		childKey: 'id'				// right table key
 	}
 ];
