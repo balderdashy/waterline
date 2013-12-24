563b89bb2db9142d7ee09db0bf06e4c9c2d3da1a
Mike McNeil <michael.r.mcneil@gmail.com>
2013-12-24 01:15:12 -0600

    Split fixures into two separate buckets for new 1..n association tests.

---
+++ test/support/fixtures/integrator/n..1.joins.js
@@ -0,0 +1,31 @@
+/**
+ * Joins
+ * 
+ * @type {Array}
+ */
+module.exports = [
+	{
+		alias: 'from',				// the `alias` -- e.g. name of association
+		parent: 'message',			// left table name
+		parentKey: 'id',			// left table primary key
+		child: 'user',				// right table name
+		childKey: 'id'				// right table foreign key
+	}
+];
+
+
+
+/*
+alias: 'to',				// the `alias` -- e.g. name of association
+
+where: { SUBQUERY_CAN_GO_HERE },		// join subcriteria
+select: [ SUBQUERY_CAN_GO_HERE ],
+limit: undefined,
+skip: undefined,
+joins: [ { SUBQUERY_CAN_GO_HERE } ],
+
+parent: 'user',				// left table name
+parentKey: 'id',			// left table primary key
+child: 'message_to_user',	// right table name
+childKey: 'user_id'			// right table foreign key
+*/
---
+++ test/support/fixtures/integrator/n..m.joins.js
@@ -0,0 +1,21 @@
+/**
+ * Joins
+ * 
+ * @type {Array}
+ */
+module.exports = [
+	{
+		alias: 'to',				// the `alias` -- e.g. name of association
+		parent: 'message',			// left table name
+		parentKey: 'id',			// left table primary key
+		child: 'message_to_user',	// right table name
+		childKey: 'message_id'		// right table foreign key
+	},
+	{
+		alias: 'to',				// the `alias` -- e.g. name of association
+		parent: 'message_to_user',	// left table name
+		parentKey: 'user_id',		// left table foreign key
+		child: 'user',				// right table name
+		childKey: 'id'				// right table primary key
+	}
+];
