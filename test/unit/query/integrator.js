bb1f660df648dab50248debcd04698b0016e66a0
Mike McNeil <michael.r.mcneil@gmail.com>
2013-12-24 01:14:49 -0600

    Fixes 1..n associations.

--- test/unit/query/integrator.js
+++ test/unit/query/integrator.js
@@ -2,10 +2,6 @@
  * Module dependencies
  */
 var integrate = require('../../../lib/waterline/query/integrator');
-var fixtures = {
-	joins: require('../../support/fixtures/integrator/joins'),
-	cache: require('../../support/fixtures/integrator/cache')
-};
 var assert = require('assert');
 var should = require('should');
 var _ = require('lodash');
@@ -44,10 +40,13 @@ describe('integrator', function () {
 
 	describe('with valid input', function () {
 
-		var results;
+		describe(':: N..M :: ',function () {
 
-
-		describe(':: results',function () {
+			var fixtures = {
+				joins: require('../../support/fixtures/integrator/n..m.joins.js'),
+				cache: require('../../support/fixtures/integrator/cache')
+			};
+			var results;
 
 			before(function (done){
 				// console.log('!!!START!!!');
@@ -65,8 +64,69 @@ describe('integrator', function () {
 				results.should.be.Array;
 			});
 
-			// TODO: need to get a hold of actual physical schema to check this
 			it('should have items which have all the properties of the parent table');
+
+			describe(':: populated aliases', function () {
+				var aliases = Object.keys(_.groupBy(fixtures.joins, 'alias'));
+
+				it('should exist for every alias specified in `joins` (i.e. every `populate()`)', function () {
+
+					// Each result is an object and contains a valid alias
+					_.each(results, function (result) {
+						result
+						.should.be.Object;
+						
+						_.any(aliases, function (alias) {
+							return result[alias];
+						})
+						.should.be.true;
+					});
+
+					// Double check.
+					_.each(results, function (result) {
+						result.should.be.Object;
+
+						_.each(aliases, function (alias) {
+							result[alias].should.be.ok;
+							result[alias].should.be.ok;
+						});
+					});
+
+					// All aliases are accounted for in results
+					_.all(aliases, function (alias) {
+						return results.length === _.pluck(results, alias).length;
+					}).should.be.true;
+				});
+			});
+		});
+
+
+
+
+
+
+
+		describe(':: 1..N ::',function () {
+
+			var results;
+			var fixtures = {
+				joins: require('../../support/fixtures/integrator/n..1.joins.js'),
+				cache: require('../../support/fixtures/integrator/cache')
+			};
+
+			before(function (done){
+				assert.doesNotThrow(function () {
+					integrate(fixtures.cache, fixtures.joins, function (err, _results) {
+						assert(!err);
+						results = _results;
+						done(err);
+					});
+				});
+			});
+
+			it('should be an array', function () {
+				results.should.be.Array;
+			});
 
 			describe(':: populated aliases', function () {
 				var aliases = Object.keys(_.groupBy(fixtures.joins, 'alias'));
