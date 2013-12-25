db561f469eca5354b8015cf63fe431ffa5702622
Mike McNeil <michael.r.mcneil@gmail.com>
2013-12-25 01:16:08 -0600

    Added doc on expected format of join instructions.

--- test/support/fixtures/integrator/n..1.joins.js
+++ test/support/fixtures/integrator/n..1.joins.js
@@ -12,20 +12,3 @@ module.exports = [
 		childKey: 'id'				// right table key
 	}
 ];
-
-
-
-/*
-alias: 'to',				// the `alias` -- e.g. name of association
-
-where: { SUBQUERY_CAN_GO_HERE },		// join subcriteria
-select: [ SUBQUERY_CAN_GO_HERE ],
-limit: undefined,
-skip: undefined,
-joins: [ { SUBQUERY_CAN_GO_HERE } ],
-
-parent: 'user',				// left table name
-parentKey: 'id',			// left table key
-child: 'message_to_user',	// right table name
-childKey: 'user_id'			// right table key
-*/
