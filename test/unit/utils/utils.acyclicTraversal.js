9c5390416c8ce7397a5dd5df2a2ae626291d130c
Mike McNeil <michael.r.mcneil@gmail.com>
2014-03-10 22:14:12 -0500

    First fixes, beginnings of test for graph traversal util.

---
+++ test/unit/utils/utils.acyclicTraversal.js
@@ -0,0 +1,45 @@
+var assert = require('assert'),
+    traverse = require('../../../lib/waterline/utils/acyclicTraversal');
+
+describe('utils/acyclicTraversal', function() {
+
+  describe('schema', function() {
+    
+    var schema = {
+      user: {
+        attributes: {
+          name: 'string',
+          age: 'integer',
+          pets: {
+            collection: 'pet',
+            via: 'owner'
+          },
+          formerPets: {
+            collection: 'user',
+            via: 'formerOwners'
+          }
+        }
+      },
+      pet: {
+        attributes: {
+          name: 'string',
+          breed: 'string',
+          owner: {
+            model: 'user'
+          },
+          formerOwners: {
+            collection: 'user',
+            via: 'formerPets'
+          }
+        }
+      }
+    };
+
+    it('should return a .populate() plan', function() {
+      var plan = traverse(schema, 'user', 'pets');
+      console.log(plan);
+      assert(typeof plan === 'object');
+    });
+  });
+
+});
