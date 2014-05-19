76d7ad8623e8180c88dff5e307f032a9061d50f5
Mike McNeil <mike@balderdash.co>
2014-05-19 02:46:07 -0500

    Now only 17 fail

--- test/unit/query/query.average.js
+++ test/unit/query/query.average.js
@@ -22,6 +22,7 @@ describe('Collection average', function () {
       // Fixture Adapter Def
       var adapterDef = {
         find: function (con, col, criteria, cb) {
+          console.log('---',arguments);
           return cb(null, [criteria]);
         }
       };
@@ -45,6 +46,7 @@ describe('Collection average', function () {
       query.find().average('age', 'percent').exec(function (err, obj) {
         if(err) return done(err);
 
+        console.log(obj);
         assert(obj[0].average[0] === 'age');
         assert(obj[0].average[1] === 'percent');
         done();
