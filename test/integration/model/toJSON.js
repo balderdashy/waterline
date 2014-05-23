9e82bed37e7a12f959a21989355ca898baab5f21
Scott Gress <scott@pigandcow.com>
2014-05-22 19:13:38 -0500

    Added a test for default toJSON removing protected attributes, then did 100 laps for checking in new functionality without writing a test for it

--- test/integration/model/toJSON.js
+++ test/integration/model/toJSON.js
@@ -8,7 +8,7 @@ describe('Model', function() {
     // TEST SETUP
     ////////////////////////////////////////////////////
 
-    var collection;
+    var collection, collection2;
 
     before(function(done) {
       var waterline = new Waterline();
@@ -28,8 +28,20 @@ describe('Model', function() {
           }
         }
       });
+      var Collection2 = Waterline.Collection.extend({
+        connection: 'my_foo',
+        tableName: 'person2',
+        attributes: {
+          first_name: {type: 'string', protected: true},
+          last_name: 'string',
+          full_name: function() {
+            return this.first_name + ' ' + this.last_name;
+          }
+        }
+      });
 
       waterline.loadCollection(Collection);
+      waterline.loadCollection(Collection2);
 
       var connections = {
         'my_foo': {
@@ -40,6 +52,7 @@ describe('Model', function() {
       waterline.initialize({ adapters: { foobar: {} }, connections: connections }, function(err, colls) {
         if(err) done(err);
         collection = colls.collections.person;
+        collection2 = colls.collections.person2;
         done();
       });
     });
@@ -56,6 +69,15 @@ describe('Model', function() {
       assert(obj === Object(obj));
       assert(obj.first_name === 'foo');
       assert(!obj.last_name);
+    });
+
+    it('should remove any attributes marked as "protected"', function() {
+      var person = new collection2._model({ first_name: 'foo', last_name: 'bar' });
+      var obj = person.toJSON();
+
+      assert(obj === Object(obj));
+      assert(!obj.first_name);
+      assert(obj.last_name == 'bar');
     });
 
   });
