d546baeb9365f726ccf7446a939c7f0b0c7e8de5
Riceball LEE <snowyu.lee@gmail.com>
2014-12-12 09:15:01 +0800

    + [feature] supports the enum/in can use object to transform value see https://github.com/balderdashy/waterline/issues/52

--- test/unit/validations/validation.enum.js
+++ test/unit/validations/validation.enum.js
@@ -3,7 +3,7 @@ var Validator = require('../../../lib/waterline/core/validations'),
 
 describe('validations', function() {
 
-  describe('enum', function() {
+  describe('enum string', function() {
     var validator;
 
     before(function() {
@@ -29,6 +29,39 @@ describe('validations', function() {
     });
 
     it('should NOT error if valid enum is set', function(done) {
+      validator.validate({ sex: 'male' }, function(errors) {
+        assert(!errors);
+        done();
+      });
+    });
+  });
+
+  describe('enum integer', function() {
+    var validator;
+
+    before(function() {
+
+      var validations = {
+        sex: {
+          type: 'integer',
+          in: {'male':0, 'female':1}
+        }
+      };
+
+      validator = new Validator();
+      validator.initialize(validations);
+    });
+
+    it('should error if invalid enum is set', function(done) {
+      validator.validate({ sex: 'other' }, function(errors) {
+        assert(errors);
+        assert(errors.sex);
+        assert(errors.sex[0].rule === 'in');
+        done();
+      });
+    });
+
+    it.only('should NOT error if valid enum is set', function(done) {
       validator.validate({ sex: 'male' }, function(errors) {
         assert(!errors);
         done();
