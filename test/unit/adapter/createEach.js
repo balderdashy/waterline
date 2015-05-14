ce9ad205e3c9edc631f33ef0384b182dc0980e30
Khalil AIFAOUI <khalil_ai@live.fr>
2015-05-14 10:38:05 +0100

    New validation method.

--- test/unit/adapter/createEach.js
+++ test/unit/adapter/createEach.js
@@ -5,23 +5,16 @@ describe('Model.createEach()', function () {
     waterline = new Waterline();
     var table;
     var records = [
-        {attr: 'createEach_1', type: 'createEach'},
-        {attr: 'createEach_2', type: 'createEach'}
+        {a: 'createEach_1', b: 'createEach'},
+        {a: 'createEach_2', b: 'createEach'}
     ];
     var Test = Waterline.Collection.extend({
         identity: 'foo',
         connection: 'my_foo',
         tableName: 'fooTable',
         attributes: {
-            "attr": {
-                "type": "string",
-                "columnName": "foo_attr"
-            },
-            "type": {
-                "type": "string",
-                "required": true,
-                "columnName": "foo_type"
-            }
+            "a":  "string",
+            "b": "string"
         }
     });
     var adapter = {
@@ -30,8 +23,8 @@ describe('Model.createEach()', function () {
         },
         describe: function (connectionName, collectionName, cb, connection) {
             var schema = {
-                foo_attr: {type: 'VARCHAR2'},
-                foo_type: {type: 'VARCHAR2'}
+                a: {type: 'VARCHAR2'},
+                b: {type: 'VARCHAR2'}
             };
             cb(null, (table && table.length) ? schema : undefined);
         },
@@ -40,12 +33,10 @@ describe('Model.createEach()', function () {
             cb(null, null);
         },
         create: function (connectionName, collectionName, data, cb) {
-            assert(false,'adapter.create() is called instead of adapter.createEach()');
             table.push(data);
-            cb(null,data);
+            cb('create called',data);
         },
         createEach: function (connectionName, collectionName, valuesList, cb, connection) {
-            assert(true);
             cb(null,valuesList);
         },
         drop: function (connectionName, collectionName, relations, cb, connection) {
@@ -60,18 +51,15 @@ describe('Model.createEach()', function () {
     };
     waterline.loadCollection(Test);
     var connections = {'my_foo': {adapter: 'adapter'}};
-    it('should call adapter.createEach()', function (done) {
+    it('should call adapter.createEach() when existing', function (done) {
         waterline.initialize({adapters: {adapter: adapter}, connections: connections}, function (err, data) {
-            data.collections.foo.createEach(records, function (err, res) {
-                if (err){
-                    return done(err);
-                }
+            if (err) return done(err);
+            data.collections.foo.createEach(records, function (createCalled, res) {
+                assert(!createCalled,'adapter.create() is called');
                 /* reinitialization to stimulate "alter" mode recovery to test adapter.createEach() call */
                 waterline.loadCollection(Test);
-                waterline.initialize({adapters: {adapter: adapter}, connections: connections}, function (err, data) {
-                    if(err){
-                        return done(err);
-                    }
+                waterline.initialize({adapters: {adapter: adapter}, connections: connections}, function (createCalled, data) {
+                    assert(!createCalled,'adapter.create() is called while reinserting in "Alter" mode');
                     done();
                 });
             });
