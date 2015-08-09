d333bee16e47082cc2e67f5cf66cc32a3890dfa7
dan sutton <danielsutton01@gmail.com>
2015-08-08 23:19:56 -0500

    beginning of refactor

--- test/unit/utils/utils.normalize.js
+++ test/unit/utils/utils.normalize.js
@@ -1,13 +1,54 @@
 var assert = require('assert'),
-    normalize = require('../../../lib/waterline/utils/normalize');
+    normalize = require('../../../lib/waterline/utils/normalize'),
+    units = normalize.NotExposed;
 
 describe("Normalize utility", function() {
 
   describe(".criteria()", function() {
 
+    describe("default checks", function() {
+      it("should return false if given false", function() {
+        var criteria = normalize.criteria(false);
+        assert(criteria === false);
+      });
+
+      it("should return arrays to the calling method", function() {
+        var criteria = [1,2,3];
+        var result = normalize.criteria(criteria);
+        assert.deepEqual(criteria, result);
+      });
+
+      it("should change undefined values to null", function() {
+        var undefinedValue;
+        var criteria = {id: undefinedValue};
+        var result = normalize.criteria(criteria);
+        assert.deepEqual({where: {id:null}}, result);
+      });
+
+      it("should wrap primitives with an id tag", function() {
+        var primitives = [1,-17,"idtag"];
+        for (var i = 0; i < primitives.length; i++) {
+          var prop = primitives[i];
+          var result = normalize.criteria(prop);
+          assert.deepEqual({where: {id: prop}}, result);
+        }
+      });
+
+      it("should not add a where clause if already present", function() {
+        var criteria = {where: {id: 4}};
+        var result = normalize.criteria(criteria);
+        assert.deepEqual(criteria, result);
+      });
+    });
+
+    // describe("Where Clause Attribute Transformation", function() {
+
+    // })
+
     describe("sort", function() {
       it("should default to asc", function() {
         var criteria = normalize.criteria({ sort: "name" });
+        console.log(criteria);
 
         assert(criteria.sort.name === 1);
       });
@@ -26,17 +67,18 @@ describe("Normalize utility", function() {
 
       it("should properly normalize valid sort", function() {
         var criteria = normalize.criteria({ sort: "name desc" });
+        console.log(criteria);
 
         assert(criteria.sort.name === -1);
       });
-      
+
       it("should properly normalize valid sort with upper case", function() {
         var criteria = normalize.criteria({ sort: "name DESC" });
-
+        console.log(criteria);
         assert(criteria.sort.name === -1);
       });
     });
-    
+
     describe("sort object", function() {
       it("should throw error on invalid order", function() {
         var error;
@@ -49,12 +91,13 @@ describe("Normalize utility", function() {
 
         assert(typeof error !== 'undefined');
       });
-      
+
       it("should properly normalize valid sort", function() {
         var criteria = normalize.criteria({ sort: { name: "asc" } });
+        console.log(criteria);
         assert(criteria.sort.name === 1);
       });
-      
+
       it("should properly normalize valid sort with upper case", function() {
         var criteria = normalize.criteria({ sort: { name: "DESC" } });
         assert(criteria.sort.name === -1);
