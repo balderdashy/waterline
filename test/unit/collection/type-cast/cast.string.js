0f860f0e48a5f061e072191cf60353374985fba6
Mike McNeil <mikermcneil@users.noreply.github.com>
2016-12-24 14:00:48 -0600

    Update string type coercion tests

--- test/unit/collection/type-cast/cast.string.js
+++ test/unit/collection/type-cast/cast.string.js
@@ -2,13 +2,15 @@ var assert = require('assert');
 var _ = require('@sailshq/lodash');
 var Waterline = require('../../../../lib/waterline');
 
-describe.skip('Collection Type Casting ::', function() {
+describe('Collection Type Casting ::', function() {
   describe('with String type ::', function() {
-    var person;
 
+    var Person;
+    var orm;
     before(function(done) {
-      var waterline = new Waterline();
-      var Person = Waterline.Model.extend({
+      orm = new Waterline();
+
+      orm.registerModel(Waterline.Model.extend({
         identity: 'person',
         datastore: 'foo',
         primaryKey: 'id',
@@ -16,35 +18,51 @@ describe.skip('Collection Type Casting ::', function() {
           id: {
             type: 'number'
           },
+          activated: {
+            type: 'boolean'
+          },
           name: {
             type: 'string'
           }
         }
-      });
+      }));
 
-      waterline.registerModel(Person);
-
-      var datastores = {
-        'foo': {
-          adapter: 'foobar'
+      orm.initialize({
+        adapters: {
+          foobar: {}
+        },
+        datastores: {
+          foo: { adapter: 'foobar' }
         }
-      };
+      }, function(err, orm) {
+        if (err) { return done(err); }
 
-      waterline.initialize({ adapters: { foobar: {} }, datastores: datastores }, function(err, orm) {
-        if(err) {
-          return done(err);
-        }
-        person = orm.collections.person;
-        done();
-      });
-    });
+        Person = orm.collections.person;
+        return done();
+      });//</.initialize()>
+
+    });//</before>
+
 
     it('should cast numbers to strings', function() {
-      var values = { name: 27 };
-      person._cast(values);
+      assert.equal(Person.validate('name', 27), '27');
+    });
 
-      assert(_.isString(values.name));
-      assert.equal(values.name, '27');
+
+    it('should throw E_VALIDATION error when a value can\'t be cast', function() {
+      try {
+        Person.validate('name', null);
+      } catch (e) {
+        switch (e.code) {
+          case 'E_VALIDATION':
+            // TODO: check more things
+            return;
+
+          // As of Thu Dec 22, 2016, this test is failing because
+          // validation is not being completely rolled up yet.
+          default: throw new Error('The actual error code was "'+e.code+'" - but it should have been "E_VALIDATION": the rolled-up validation error.  This is so that errors from the public `.validate()` are consistent with errors exposed when creating or updating records (i.e. when multiple values are being set at the same time.)  Here is the error that was actually received:\n```\n' +e.stack+'\n```');
+        }
+      }
     });
 
   });
