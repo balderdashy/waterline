af75a29519c453eb4ca5dec7ec45483adb715fd9
Mike McNeil <mikermcneil@users.noreply.github.com>
2017-01-05 16:41:34 -0600

    Fix typo and get tests to run.

--- test-2.js
+++
@@ -1,3 +0,0 @@
-
-
-require('./test-error')();
--- test-error.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = function foo(){
-
-  var err = new Error('in test-error');
-  Error.captureStackTrace(err, foo);
-  throw err;
-
-};
