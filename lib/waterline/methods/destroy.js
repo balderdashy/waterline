balderdashy/waterline

commit 2e89df04d60c90347628f80a396d441c112106cb

When 'fetch: true' is not in use, tolerate the null literal coming back from the adapter as the raw result.

lib/waterline/methods/destroy.js
@@ -435,7 +435,14 @@ module.exports = function destroy(/* criteria, explicitCbMaybe, metaContainer */
                 // If `fetch` was not enabled, return.
                 if (!_.has(query.meta, 'fetch') || query.meta.fetch === false) {
 
-                  if (!_.isUndefined(rawAdapterResult) && _.isArray(rawAdapterResult)) {
+                  // > Note: This `if` statement is a convenience, for cases where the result from
+                  // > the adapter may have been coerced from `undefined` to `null` automatically.
+                  // > (we want it to be `undefined` still, for consistency)
+                  if (_.isNull(rawAdapterResult)) {
+                    return done();
+                  }//-•
+
+                  if (!_.isUndefined(rawAdapterResult)) {
                     console.warn('\n'+
                       'Warning: Unexpected behavior in database adapter:\n'+
                       'Since `fetch` is NOT enabled, this adapter (for datastore `'+WLModel.datastore+'`)\n'+

lib/waterline/methods/create-each.js
@@ -273,6 +273,13 @@ module.exports = function createEach( /* newRecords?, explicitCbMaybe?, meta? */
         var fetch = modifiedMeta || (_.has(query.meta, 'fetch') && query.meta.fetch);
         if (!fetch) {
 
+          // > Note: This `if` statement is a convenience, for cases where the result from
+          // > the adapter may have been coerced from `undefined` to `null` automatically.
+          // > (we want it to be `undefined` still, for consistency)
+          if (_.isNull(rawAdapterResult)) {
+            return done();
+          }//-•
+
           if (!_.isUndefined(rawAdapterResult)) {
             console.warn('\n'+
               'Warning: Unexpected behavior in database adapter:\n'+

lib/waterline/methods/create.js
@@ -275,6 +275,13 @@ module.exports = function create(newRecord, explicitCbMaybe, metaContainer) {
           var fetch = modifiedMeta || (_.has(query.meta, 'fetch') && query.meta.fetch);
           if (!fetch) {
 
+            // > Note: This `if` statement is a convenience, for cases where the result from
+            // > the adapter may have been coerced from `undefined` to `null` automatically.
+            // > (we want it to be `undefined` still, for consistency)
+            if (_.isNull(rawAdapterResult)) {
+              return done();
+            }//-•
+
             if (!_.isUndefined(rawAdapterResult)) {
               console.warn('\n'+
                 'Warning: Unexpected behavior in database adapter:\n'+

lib/waterline/methods/update.js
@@ -279,7 +279,14 @@ module.exports = function update(criteria, valuesToSet, explicitCbMaybe, metaCon
           // If `fetch` was not enabled, return.
           if (!_.has(query.meta, 'fetch') || query.meta.fetch === false) {
 
-            if (!_.isUndefined(rawAdapterResult) && _.isArray(rawAdapterResult)) {
+            // > Note: This `if` statement is a convenience, for cases where the result from
+            // > the adapter may have been coerced from `undefined` to `null` automatically.
+            // > (we want it to be `undefined` still, for consistency)
+            if (_.isNull(rawAdapterResult)) {
+              return done();
+            }//-•
+
+            if (!_.isUndefined(rawAdapterResult)) {
               console.warn('\n'+
                 'Warning: Unexpected behavior in database adapter:\n'+
                 'Since `fetch` is NOT enabled, this adapter (for datastore `'+WLModel.datastore+'`)\n'+

commit abcaf85c263b94fed581e5db95d2f3f80ae2af86

Fix copy/paste error introduced in 2e89df04d60c90347628f80a396d441c112106cb

lib/waterline/methods/destroy.js
@@ -439,7 +439,7 @@ module.exports = function destroy(/* criteria, explicitCbMaybe, metaContainer */
                   // > the adapter may have been coerced from `undefined` to `null` automatically.
                   // > (we want it to be `undefined` still, for consistency)
                   if (_.isNull(rawAdapterResult)) {
-                    return done();
+                    return proceed();
                   }//-•
 
                   if (!_.isUndefined(rawAdapterResult)) {