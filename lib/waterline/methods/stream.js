balderdashy/waterline

commit d1811c625e8561f9203cb618afa0784891958865

Made .stream() batch size configurable via batchSize meta key

lib/waterline/methods/stream.js
@@ -249,13 +249,11 @@ module.exports = function stream( /* criteria?, eachRecordFn?, explicitCbMaybe?,
       //  ││││ ││││  ╠═╣║   ║ ║ ║╠═╣║  ║ ╚╦╝   │ ├─┤│  ├┴┐   │ │ │   │ ├─┤├┤    ││├┴┐└─┐
       //  ┘└┘└─┘└┴┘  ╩ ╩╚═╝ ╩ ╚═╝╩ ╩╩═╝╩═╝╩    ┴ ┴ ┴┴─┘┴ ┴   ┴ └─┘   ┴ ┴ ┴└─┘  ─┴┘└─┘└─┘
       //
-      // When running a `.stream()`, Waterline grabs pages (batches) of like 30 records at a time.
-      // This is not currently configurable.
-      //
-      // > If you have a use case for changing this page size (batch size) dynamically, please
-      // > create an issue with a detailed explanation.  Wouldn't be hard to add, we just
-      // > haven't run across a need to change it yet.
-      var BATCH_SIZE = 30;
+      // When running a `.stream()`, Waterline grabs batches (pages) of 30
+      // records at a time, by default.  (This can be overridden using the
+      // "batchSize" meta key.)
+      var DEFAULT_BATCH_SIZE = 30;
+      var batchSize = (query.meta && query.meta.batchSize !== undefined) ? query.meta.batchSize : DEFAULT_BATCH_SIZE;
 
       // A flag that will be set to true after we've reached the VERY last batch.
       var reachedLastBatch;
@@ -275,9 +273,9 @@ module.exports = function stream( /* criteria?, eachRecordFn?, explicitCbMaybe?,
         // 30  => 15
         // 45  => 5
         // 50
-        var numRecordsLeftUntilAbsLimit = query.criteria.limit - ( i*BATCH_SIZE );
-        var limitForThisBatch = Math.min(numRecordsLeftUntilAbsLimit, BATCH_SIZE);
-        var skipForThisBatch = query.criteria.skip +  ( i*BATCH_SIZE );
+        var numRecordsLeftUntilAbsLimit = query.criteria.limit - ( i*batchSize );
+        var limitForThisBatch = Math.min(numRecordsLeftUntilAbsLimit, batchSize);
+        var skipForThisBatch = query.criteria.skip +  ( i*batchSize );
         //                     |_initial offset    +  |_relative offset from end of previous batch
 
 
@@ -298,7 +296,7 @@ module.exports = function stream( /* criteria?, eachRecordFn?, explicitCbMaybe?,
         };
         // console.log('---iterating---');
         // console.log('i:',i);
-        // console.log('   BATCH_SIZE:',BATCH_SIZE);
+        // console.log('   batchSize:',batchSize);
         // console.log('   query.criteria.limit:',query.criteria.limit);
         // console.log('   query.criteria.skip:',query.criteria.skip);
         // console.log('   query.criteria.sort:',query.criteria.sort);

lib/waterline/utils/query/forge-stage-two-query.js
@@ -14,6 +14,7 @@ var normalizeCriteria = require('./private/normalize-criteria');
 var normalizeNewRecord = require('./private/normalize-new-record');
 var normalizeValueToSet = require('./private/normalize-value-to-set');
 var buildUsageError = require('./private/build-usage-error');
+var isSafeNaturalNumber = require('./private/is-safe-natural-number');
 
 
 /**
@@ -393,14 +394,12 @@ module.exports = function forgeStageTwoQuery(query, orm) {
   })();
 
 
-  //  ┬  ┬┌─┐┬  ┬┌┬┐┌─┐┌┬┐┌─┐  ┌─┐┌─┐┌┬┐┌┬┐┌─┐┌┐┌  ┌┬┐┌─┐┌┬┐┌─┐  ┬┌─┌─┐┬ ┬┌─┐
-  //  └┐┌┘├─┤│  │ ││├─┤ │ ├┤   │  │ ││││││││ ││││  │││├┤  │ ├─┤  ├┴┐├┤ └┬┘└─┐
-  //   └┘ ┴ ┴┴─┘┴─┴┘┴ ┴ ┴ └─┘  └─┘└─┘┴ ┴┴ ┴└─┘┘└┘  ┴ ┴└─┘ ┴ ┴ ┴  ┴ ┴└─┘ ┴ └─┘
-  // Next, check specific `meta` keys, to make sure they're valid.
-  // (Not all `meta` keys can be checked, obviously, because there could be **anything**
-  // in there, such as meta keys proprietary to particular adapters.  But certain core
-  // `meta` keys can be properly verified.  Currently, we only validate _some_ of the
-  // ones that are more commonly used.)
+  // Next, check specific, common `meta` keys, to make sure they're valid.
+  // > (Not all `meta` keys can be checked, obviously, because there could be **anything**
+  // > in there, such as meta keys proprietary to particular adapters.  But certain core
+  // > `meta` keys can be properly verified.  Currently, we only validate _some_ of the
+  // > ones that are more commonly used.)
+
   if (query.meta !== undefined) {
 
     //  ┌─┐┌─┐┌┬┐┌─┐┬ ┬
@@ -420,6 +419,7 @@ module.exports = function forgeStageTwoQuery(query, orm) {
       // make sure that the `fetch` meta key hasn't been explicitly set
       // (because that wouldn't make any sense).
       if (_.contains(['findOrCreate', 'updateOne', 'destroyOne', 'archiveOne'], query.method)) {
+        // FUTURE: consider changing this usage error to a warning instead.
         throw buildUsageError(
           'E_INVALID_META',
           'The `fetch` meta key should not be provided when calling .'+query.method+'().  '+
@@ -504,6 +504,31 @@ module.exports = function forgeStageTwoQuery(query, orm) {
         throw buildUsageError(
           'E_INVALID_META',
           'If provided, `skipEncryption` should be true or false.',
+          query.using
+        );
+      }//•
+
+    }//ﬁ
+
+    //  ┌┐ ┌─┐┌┬┐┌─┐┬ ┬┌─┐┬┌─┐┌─┐
+    //  ├┴┐├─┤ │ │  ├─┤└─┐│┌─┘├┤
+    //  └─┘┴ ┴ ┴ └─┘┴ ┴└─┘┴└─┘└─┘
+    if (query.meta.batchSize !== undefined) {
+
+      if (!_.isNumber(query.meta.batchSize) || !isSafeNaturalNumber(query.meta.batchSize)) {
+        throw buildUsageError(
+          'E_INVALID_META',
+          'If provided, `batchSize` should be a whole, positive, safe, and natural integer.',
+          query.using
+        );
+      }//•
+
+      if (query.method !== 'stream') {
+        // FUTURE: consider changing this usage error to a warning instead.
+        throw buildUsageError(
+          'E_INVALID_META',
+          '`batchSize` cannot be used with .'+query.method+'() -- it is only compatible '+
+          'with the .stream() model method.',
           query.using
         );
       }//•

commit dc193885a3ac10887522df106ae1124a3eb50e00

Avoid accidentally tripping assertion introduced in d1811c625e8561f9203cb618afa0784891958865 + slight improvement to usage error msg

lib/waterline/methods/stream.js
@@ -318,8 +318,10 @@ module.exports = function stream( /* criteria?, eachRecordFn?, explicitCbMaybe?,
         });
 
         // Pass through `meta` so we're sure to use the same db connection
-        // and settings (i.e. esp. relevant if we happen to be inside a transaction)
-        deferredForThisBatch.meta(query.meta);
+        // and settings (esp. relevant if we happen to be inside a transaction).
+        // > Note that we trim out `batchSize` to avoid tripping assertions about
+        // > method compatibility.
+        deferredForThisBatch.meta(query.meta ? _.omit(query.meta, ['batchSize']) : undefined);
 
         deferredForThisBatch.exec(function (err, batchOfRecords){
           if (err) { return next(err); }

lib/waterline/utils/query/forge-stage-two-query.js
@@ -518,7 +518,8 @@ module.exports = function forgeStageTwoQuery(query, orm) {
       if (!_.isNumber(query.meta.batchSize) || !isSafeNaturalNumber(query.meta.batchSize)) {
         throw buildUsageError(
           'E_INVALID_META',
-          'If provided, `batchSize` should be a whole, positive, safe, and natural integer.',
+          'If provided, `batchSize` should be a whole, positive, safe, and natural integer.  '+
+          'Instead, got '+util.inspect(query.meta.batchSize, {depth: null})+'.',
           query.using
         );
       }//•